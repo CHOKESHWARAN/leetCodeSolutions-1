9c8ecf3b2c90bdb6beee467caf9e602f5a0b8daa
CHOKESHWARAN <chokesh1906@gmail.com>
2026-07-22 11:42:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (98.04%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int divide(int di, int d) {
+     if(d==-1&&di==-2147483648){
+         return 2147483647;}
+    int s=di/d;
+    
+    
+    return s;
+    
+   
+    }
+};

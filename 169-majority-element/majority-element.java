6aac2feaa45ac8b64c9de7faf2eefa481bb5a2e4
CHOKESHWARAN <chokesh1906@gmail.com>
2026-07-31 18:26:07 +0530

    Time: 1 ms (99.89%) | Memory: 55.8 MB (32.15%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int m=0;
+        int c=0;
+        for(int n:nums){
+            if(c==0){
+                m=n;
+            }
+            if(m==n){
+                c++;
+            }
+            else{
+                c--;
+            }
+        }
+    return m;
+    }
+}

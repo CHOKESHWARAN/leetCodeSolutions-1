c4ab6a30b273206d36c2fa08331359d14f5340c7
CHOKESHWARAN <chokesh1906@gmail.com>
2026-08-03 13:11:15 +0530

    Time: 24 ms (27.53%) | Memory: 81.6 MB (79.86%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,16 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums[0]==984)
-             return true;
-        if(nums.length-1==99999)
-            return false;
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j])
-                   return true;
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
             }
         }
+        
         return false;
-       
     }
 }
